420cd9bf493f40b92119869e93efbd2624f3f6d8
smokeys_house <leonp1mcg@gmail.com>
2019-06-02 11:06:58 -0400

    Initial Commit

---
+++ Source/BOHICA/BOHICAGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BOHICAGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BOHICA_API ABOHICAGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
