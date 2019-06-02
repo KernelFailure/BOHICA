6699c68795218f1f920d0d0b449c69d0404f06bc
smokeys_house <leonp1mcg@gmail.com>
2019-06-02 17:45:06 -0400

    Added GetControlledTank in cpp

--- Source/BOHICA/Private/TankPlayerController.cpp
+++ Source/BOHICA/Private/TankPlayerController.cpp
@@ -3,3 +3,15 @@
 
 #include "TankPlayerController.h"
 
+ATank* ATankPlayerController::GetControlledTank() const {
+    // auto tank = Cast<ATank>(GetPawn());
+    // tank->GetName();
+    return Cast<ATank>(GetPawn());
+}
+
+void ATankPlayerController::BeginPlay()  {
+    Super::BeginPlay();
+    auto tank = GetControlledTank();
+    if (!tank) {return;}
+    UE_LOG(LogTemp, Warning, TEXT("Player controller found tank: %s"), *(tank->GetName()));
+}
